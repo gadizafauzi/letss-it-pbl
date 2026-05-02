4911a27c49058acd123527ceb226758e6d5feee7
Gadiza Fauzi <gadizafauzii@gmail.com>
2026-05-02 22:05:01 +0700

    feat(routes): menambahkan routing login dan dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,53 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+
+
+// Route::middleware(['auth'])->group(function () {
+
+//     Route::get('/admin/dashboard', function () {
+//         return view('admin.dashboard');
+//     })->name('admin.dashboard');
+
+//     Route::get('/teacher/dashboard', function () {
+//         return view('teacher.dashboard');
+//     })->name('teacher.dashboard');
+
+//     Route::get('/student/dashboard', function () {
+//         return view('student.dashboard');
+//     })->name('student.dashboard');
+
+// });
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', fn() => view('admin.dashboard'))
+        ->name('admin.dashboard');
+});
+
+Route::middleware(['auth', 'role:teacher'])->group(function () {
+    Route::get('/teacher/dashboard', fn() => view('teacher.dashboard'))
+        ->name('teacher.dashboard');
+});
+
+Route::middleware(['auth', 'role:student'])->group(function () {
+    Route::get('/student/dashboard', fn() => view('student.dashboard'))
+        ->name('student.dashboard');
+});
+
+require __DIR__ . '/auth.php';
