39ea039faabed29b25233ad2dfd1bb003484b142
Gadiza Fauzi <gadizafauzii@gmail.com>
2026-05-05 10:56:33 +0700

    feat: Mengupdate halaman login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,59 +1,64 @@
-{{-- <!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-
-<h2>Login</h2>
-
-<form method="POST" action="/login">
-    @csrf
-
-    <input type="text" name="username" placeholder="Username">
-    <br><br>
-
-    <input type="password" name="password" placeholder="Password">
-    <br><br>
-
-    <button type="submit">Login</button>
-
-    @if ($errors->any())
-        <div style="color:red;">
-            {{ $errors->first() }}
-        </div>
-    @endif
-</form>
-
-</body>
-</html> --}}
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
+    <meta charset="UTF-8">
     <title>Login</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/js/all.min.js"></script>
+
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 </head>
+
 <body>
 
-<h2>Login</h2>
+    <div class="card">
 
-@if ($errors->any())
-    <div style="color:red;">
-        {{ $errors->first() }}
+        <img src="/images/logo.jpeg" class="logo">
+        <h2 class="title">Login</h2>
+
+        <div class="tabs">
+            <button type="button" class="tab active" data-role="student">Siswa</button>
+            <button type="button" class="tab" data-role="teacher">Guru</button>
+            <button type="button" class="tab" data-role="admin">Admin</button>
+        </div>
+
+        @if ($errors->any())
+            <div class="error">{{ $errors->first() }}</div>
+        @endif
+
+        <form method="POST" action="{{ route('login') }}" id="loginForm">
+            @csrf
+
+            <input type="hidden" name="role" id="role" value="student">
+
+            <div class="input-group">
+                <i class="fa fa-user icon-left"></i>
+                <input type="text" name="login" id="login" placeholder="Masukkan NIS / NISN">
+            </div>
+
+            <div class="input-group">
+                <i class="fa fa-lock icon-left"></i>
+                <input type="password" name="password" id="password" placeholder="Password">
+                <i class="fa fa-eye icon-right" id="togglePassword"></i>
+            </div>
+
+            <button class="submit" id="loginBtn">
+                <span>Masuk</span>
+            </button>
+        </form>
+
+        <a href="{{ route('password.request') }}" style="font-size:12px; display:block; margin-bottom:10px;">
+            Lupa Password?
+        </a>
+
     </div>
-@endif
 
-<form method="POST" action="{{ route('login') }}">
-    @csrf
-
-    <label>Login (Username / NIP / NISN)</label><br>
-    <input type="text" name="login"><br><br>
-
-    <label>Password</label><br>
-    <input type="password" name="password"><br><br>
-
-    <button type="submit">Login</button>
-</form>
+    <script src="{{ asset('js/login.js') }}"></script>
 
 </body>
+
 </html>
